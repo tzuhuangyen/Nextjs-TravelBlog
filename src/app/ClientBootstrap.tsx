7a09d402246c5fb6f4a589ef53a1fad9c6c2c634
tzuhuangyen <912yan@gmail.com>
2025-03-13 18:51:47 +0700

    eslintrc/BS

--- src/app/ClientBootstrap.tsx
+++ src/app/ClientBootstrap.tsx
@@ -4,7 +4,8 @@ import { useEffect } from 'react';
 export default function ClientBootstrap() {
   useEffect(() => {
     // 只在客户端加载 Bootstrap JS
-    import('bootstrap/dist/js/bootstrap.bundle.min.js');
+    // 使用 void 操作符忽略 Promise 的返回值，并告诉 TypeScript 我们不关心类型
+    void import('bootstrap/dist/js/bootstrap.bundle.min.js' as any);
   }, []);
 
   return null; // 这个组件不渲染任何内容
